af39bd137e5383f03067882dcdd4608dfc2f2ad2
Hudakasim <hudaahmad0708@gmail.com>
2025-06-01 22:50:08 +0300

    firbase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/settings.dart';
 import 'screens/home_page/home.dart';
 
-void main() {
-    runApp(const Pomodoro());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const Pomodoro());
 }
+
 
 class Pomodoro extends StatelessWidget {
     const Pomodoro({super.key});
@@ -24,13 +31,3 @@ class Pomodoro extends StatelessWidget {
         );
     }
 }
-
-// class HomePage extends StatelessWidget {
-//     const HomePage({super.key});
-
-//     @override
-//     Widget build(BuildContext context) {
-//         return const BasePage(title: 'Ana Sayfa', content: 'Ana Sayfa');
-//     }
-// }
-
